9d325d0cbf34559a31c17b868751d2ea0941f8e4
codingSlate <ananta2021k@gmail.com>
2023-02-15 11:35:21 +0530

    react-router-dom installed, BrowserRouter, Routes, Route & pages created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import {Products} from './pages/Products'
+import {Product} from './pages/Product'
+import {Cart} from './pages/Cart'
+import {NotFound} from './pages/NotFound'
 
 function App() {
   return (
