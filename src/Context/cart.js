e4d37bccacce02afa7725812a51f47049f8fb4a7
codingSlate <ananta2021k@gmail.com>
2023-02-16 08:37:23 +0530

    CartProvider functionality added

--- src/Context/cart.js
+++ src/Context/cart.js
@@ -5,8 +5,8 @@ const initialState = {
     cartItemCount: ()=>0,
     addToCart: ()=>null,
     removeFromCart:()=>null,
-    incQuentity: ()=>null, 
-    decQuentity: ()=>null
+    incQuantity: ()=>null, 
+    decQuantity: ()=>null
 }
 
 const CartContext = createContext(initialState);
@@ -16,5 +16,38 @@ const useCart= ()=>useContext(CartContext)
 // provider 
 const CartProvider = ({childern}) =>{
     const [cart, setCart] = useState(initialState.cart)
-   
+
+    const cartItemCount =()=>{
+        return cart.reduce((acc,item)=> acc + item.quantity, 0)
+    }
+
+    const addToCart = (product)=>{
+        const productId = cart.findIndex(item=>item.product.id === product.id)
+        if(productId !== -1){
+            incQuantity(product.id)
+        }else{
+            setCart([...cart, {product, quantity:1}])
+        }
+    }
+
+    const removeFromCart=()=>{
+        setCart(cart.filter(item=>item.product.id !== product.id))
+    }
+
+    const incQuantity = () =>{
+        const copy = cart.slice()
+        const productId = copy.findIndex(item=>item.product.id === product.id)
+        if(productId !== -1){
+            copy[productId].quantity += 1
+            setCart(copy)
+        }
+    }
+    const decQuantity = () =>{
+        const copy = cart.slice()
+        const productId = copy.findIndex(item=>item.product.id === product.id)
+        if(productId !== -1){
+            copy[productId].quantity -= 1
+            setCart(copy)
+        }
+    }
 }
